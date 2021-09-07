7938727495d4851c15781ca62b46154c93e344de
feruzaxabibova99 <feruzaaa1007@gmail.com>
2021-09-07 14:30:27 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var intro = alert('uzasbo.mdm.uz saytiga xush kelibsiz!');
+var info = alert('Ozingiz haqingizda ma`lumot bering.');
+
+var firstName = prompt('Ismingizni yozing!').trim();
+var lastName = prompt('Familiyangizni yozing!').trim();
+var age = prompt('Yoshingiz nechida!').trim();
+var yourInformation = prompt('Ma`lumotingiz!').trim();
+var text = prompt('Uzasbo dasturida ishlaganmisiz?').trim();
+var link = alert('Batafsil ma`lumotlar: uzasbo.mdm.uz saytidan olasiz!');
+
+console.log(` Ismingiz: ${firstName}\n Familiyangiz: ${lastName}\n Yoshingiz: ${age}\n Ma\`lumotingiz: ${yourInformation}`);
+
+
+
